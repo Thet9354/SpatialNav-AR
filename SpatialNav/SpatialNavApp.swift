d73349f97a9aa828b5b0510e401f11c490891632
Thet Pine <thetpine254@gmail.com>
2026-07-03 15:51:26 +0800

    Initial Commit

---
+++ SpatialNav/SpatialNavApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialNavApp.swift
+//  SpatialNav
+//
+//  Created by Phoon Thet Pine on 3/7/26.
+//
+
+import SwiftUI
+
+@main
+struct SpatialNavApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpatialNavTests/SpatialNavTests.swift
@@ -0,0 +1,19 @@
+//
+//  SpatialNavTests.swift
+//  SpatialNavTests
+//
+//  Created by Phoon Thet Pine on 3/7/26.
+//
+
+import Testing
+@testable import SpatialNav
+
+struct SpatialNavTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+        // Swift Testing Documentation
+        // https://developer.apple.com/documentation/testing
+    }
+
+}
